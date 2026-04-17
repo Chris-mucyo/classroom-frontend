6f342669eb198453fab16252f189b869679bb2c1
Chris-mucyo <chrisxeventy6@gmail.com>
2026-04-17 09:33:58 +0200

    implemented data providers

--- src/providers/dataProvider.ts
+++ src/providers/dataProvider.ts
@@ -1,5 +1,22 @@
-import { DataProvider } from "@refinedev/core";
-
+import { BaseRecord, DataProvider, GetListParams, GetListResponse } from "@refinedev/core";
+import { mockSubjects } from "@/constants/MOCK_SUBJECTS";
 export const dataProvider: DataProvider = {
-    getList: async
+    getList: async <TDATA extends BaseRecord = BaseRecord>({ resource }:
+    GetListParams): Promise<GetListResponse<TDATA>> => {
+        if( resource !== 'subjects'){
+            return { data: [] as TDATA[], total: 0 };
+        }
+
+        return {
+            data: mockSubjects as unknown as TDATA[],
+            total: mockSubjects.length,
+        }
+    },
+
+    getOne: async () => {throw new Error('this is not present in mock data provider') },
+    create: async () => {throw new Error('this is not present in mock data provider') },
+    update: async () => {throw new Error('this is not present in mock data provider') },
+    deleteOne: async () => {throw new Error('this is not present in mock data provider') },
+   
+    getApiUrl: () => '',
 }
